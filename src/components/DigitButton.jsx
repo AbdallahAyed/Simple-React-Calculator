be081aeaaeec9c4961452e0dfa89771e419b1ca5
Abdallah <abdalla.ayed.2010@gmail.com>
2023-02-06 01:56:32 +0200

    fix actions location

--- src/components/DigitButton.jsx
+++ src/components/DigitButton.jsx
@@ -1,4 +1,4 @@
-import { ACTIONS } from "../App";
+import { ACTIONS } from "../logic/reducer";
 
 export default function DigitButton({ dispatch, digit }) {
   return (
--- src/components/OperationButton.jsx
+++ src/components/OperationButton.jsx
@@ -1,4 +1,4 @@
-import { ACTIONS } from "../App";
+import { ACTIONS } from "../logic/reducer";
 
 export default function OperationButton({ dispatch, operation }) {
   return (
